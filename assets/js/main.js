aab21be331a04ccf8dd3c0f862d7b17daf7c703e
Tomi Illes <tomi@Tomis-MacBook-Pro.local>
2021-03-02 22:14:50 +0100

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,7 @@
 				$("#navi ul").css('margin-top',-0.9*(1280-win_width)/544+"em");
 			}
 			else {
+				$("#nav_top ul li").css('margin','0 1em')
 				$("#navi").css('height',Math.round(736*0.113));
 				$("#header").css('height',Math.round(736*0.47));
 			}
